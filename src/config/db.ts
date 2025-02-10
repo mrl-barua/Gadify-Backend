90214b83e93fceccf2361b5adfda5711b0baf89c
mrl-barua <mlbarua@usep.edu.ph>
2025-02-10 21:54:41 +0800

    feat: update database configuration to support production environment with new DB_URL

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,18 +1,25 @@
 import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
 
-// Load environment variables from .env file
+// Load environment variables
 dotenv.config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME as string,
-  process.env.DB_USER as string,
-  process.env.DB_PASSWORD as string,
-  {
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT as any, 
-    logging: false,
-  }
-);
+const isProduction = process.env.IS_PRODUCTION === "true";
+
+const sequelize = isProduction
+  ? new Sequelize(process.env.DB_URL as string, {
+      logging: false,
+    })
+  : new Sequelize(
+      process.env.DB_NAME as string,
+      process.env.DB_USER as string,
+      process.env.DB_PASSWORD as string,
+      {
+        host: process.env.DB_HOST,
+        port: Number(process.env.DB_PORT) || 3306,
+        dialect: process.env.DB_DIALECT as any,
+        logging: false,
+      }
+    );
 
 export default sequelize;
