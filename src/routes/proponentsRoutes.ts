1fa5401cfaea4fbd7f6a7675d77f9ff97870a6d8
mrl-barua <mlbarua@usep.edu.ph>
2025-04-13 22:11:04 +0800

    feat: implement search and retrieval routes for proponents with filtering options

--- src/routes/proponentsRoutes.ts
+++ src/routes/proponentsRoutes.ts
@@ -2,6 +2,10 @@ import { Router } from "express";
 
 import {
   GetAllProponents,
+  SearchProponents,
+  GetAllApprovedProponents,
+  GetAllPendingProponents,
+  GetAllRejectedProponents,
   CreateProponents,
   DeleteProponents,
   UpdateProponents,
@@ -11,6 +15,10 @@ import {
 const router = Router();
 
 router.get("/proponents", GetAllProponents);
+router.post("/searchProponents", SearchProponents);
+router.post("/pendingProponents", GetAllPendingProponents);
+router.get("/approvedProponents", GetAllApprovedProponents);
+router.get("/rejectedProponents", GetAllRejectedProponents);
 router.post("/proponents", CreateProponents);
 router.put("/updateProponents", UpdateProponents);
 router.delete("/deleteProponents", DeleteProponents);
